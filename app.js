40ebd6d2cce06bde7a5c6126a6ef09cfe9046cb9
Zubair Ahmed <zubair1024@gmail.com>
2019-09-26 01:31:56 +0400

    adding screensaver logic

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-document.addEventListener(
-  "DOMContentLoaded",
+let mouseTimeout;
+let screensaverActive = false;
+let idleTime = 5;
+let quotesContainer = document.getElementsByClassName("quotes-container")[0];
+let activeContainer = document.getElementsByClassName("active-container")[0];
+let body = document.getElementsByTagName("body")[0];
+
+function showScreenSaver() {
+  quotesContainer.classList.remove("hidden");
+  activeContainer.classList.add("hidden");
+  screensaverActive = true;
+}
+
+function stopScreenSaver() {
+  activeContainer.classList.remove("hidden");
+  quotesContainer.classList.add("hidden");
+  screensaverActive = false;
+}
+
+body.addEventListener(
+  "mousemove",
   function() {
-    App = {
-      init: function() {}
-    };
-    App.init();
+    clearTimeout(mouseTimeout);
+
+    if (screensaverActive) {
+      stopScreenSaver();
+    }
+
+    mouseTimeout = setTimeout(function() {
+      showScreenSaver();
+    }, 1000 * idleTime);
   },
-  false
+  0
 );
